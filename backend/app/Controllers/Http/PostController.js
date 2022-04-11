e2e43ad644d0710320cf1f71c8eb6e8cfd9ae5c7
Wanderson Chaves <wandersonscpibr@gmail.com>
2022-04-11 00:51:01 -0300

    add backend and frontend - first day

---
+++ backend/app/Controllers/Http/PostController.js
@@ -0,0 +1,114 @@
+"use strict";
+
+const Post = use("App/Models/Post");
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with posts
+ */
+class PostController {
+  /**
+   * Show a list of all posts.
+   * GET posts
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index() {
+    const posts = await Post.all();
+
+    return posts;
+  }
+
+  /**
+   * Render a form to be used for creating a new post.
+   * GET posts/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create({ request, response, view }) {}
+
+  /**
+   * Create/save a new post.
+   * POST posts
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request }) {
+    const data = request.only(["content"]);
+    const post = await Post.create(data);
+
+    return post;
+  }
+
+  /**
+   * Display a single post.
+   * GET posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params }) {
+    const post = await Post.find(params.id);
+
+    return post;
+  }
+
+  /**
+   * Render a form to update an existing post.
+   * GET posts/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit({ params, request, response, view }) {}
+
+  /**
+   * Update post details.
+   * PUT or PATCH posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update({ params, request }) {
+    const data = request.only(["content"]);
+    const post = await Post.find(params.id);
+
+    post.merge(data);
+
+    await post.save();
+
+    return post;
+  }
+
+  /**
+   * Delete a post with id.
+   * DELETE posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({ params }) {
+    const post = await Post.find(params.id);
+
+    await post.delete();
+  }
+}
+
+module.exports = PostController;
